e2b391a01ac09a3b673982d871944f12ab010843
Tedde Lundgren <mail@tedeh.net>
2016-05-26 12:01:18 +0200

    feat(): add urlPath option to define the base url for doc serving

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ var rename = require('gulp-rename');
 var server = require('gulp-webserver');
 var template = require('gulp-template');
 var uglify = require('gulp-uglify');
+var debug = require('gulp-debug');
 
 var util = require('./util');
 
@@ -29,12 +30,14 @@ var appDir = path.join(__dirname, 'app');
  * (*angular*, *angular-animate*, *angular-route*, *angular-aria*, and *angular-messages* are automatically loaded)
  * @param {Array}  config.moduleJs Module JavaScript files
  * @param {Array}  config.moduleCss Module CSS files
- * @param {string} [config.outPath="dist/docs"] Output path where generated docs are placed
+ * @param {string} [config.urlPath="/docs"] URL path with leading slash that serves the generated documents
+ * @param {string} [config.outPath="dist/docs"] Output path where generated docs are located
  * @param {string} [config.repositoryUrl] Repository base URL
  * @param {boolean} [config.debug=false] Debug mode
  */
 module.exports = function (gulp, config) {
-  var outPath = config.outPath || 'dist/docs';
+  config.outPath = config.outPath || 'dist/docs';
+  config.urlPath = config.urlPath || '/docs';
 
   gulp = require('gulp-help')(gulp);
 
@@ -51,7 +54,7 @@ module.exports = function (gulp, config) {
   });
 
   gulp.task('docs:clean', false, function () {
-    return del(outPath);
+    return del(config.outPath);
   });
 
   // Parses ngDocs
@@ -60,7 +63,7 @@ module.exports = function (gulp, config) {
       .config(function(log, readFilesProcessor, writeFilesProcessor) {
         log.level = config.debug ? 'info' : 'error';
         readFilesProcessor.basePath = config.basePath;
-        writeFilesProcessor.outputFolder = outPath;
+        writeFilesProcessor.outputFolder = config.outPath;
       })
       .config(function (componentDataProcessor) {
         componentDataProcessor.repositoryUrl = config.repositoryUrl;
@@ -76,7 +79,7 @@ module.exports = function (gulp, config) {
       '!' + appDir + '/partials/**/*.html',
       '!' + appDir + '/index.html'
     ])
-      .pipe(gulp.dest(outPath));
+      .pipe(gulp.dest(config.outPath));
   });
 
   // Generates AngularJS config constants
@@ -87,23 +90,23 @@ module.exports = function (gulp, config) {
       stream: true
     })
       .pipe(rename('config-data.js'))
-      .pipe(gulp.dest(outPath + '/js'));
+      .pipe(gulp.dest(config.outPath + '/js'));
   });
 
   // Concatenates and uglifies JS
   gulp.task('docs:js', false, ['docs:app', 'docs:config', 'docs:html2js', 'docs:dgeni', 'docs:demos:data'], function() {
-    return gulp.src(outPath + '/js/**/*.js')
+    return gulp.src(config.outPath + '/js/**/*.js')
       .pipe(ngAnnotate())
       .pipe(concat('docs.js'))
       .pipe(gulpif(!config.debug, uglify()))
-      .pipe(gulp.dest(outPath));
+      .pipe(gulp.dest(config.outPath));
   });
 
   // Concatenates CSS
   gulp.task('docs:css', false, ['docs:app'], function() {
     return gulp.src(appDir + '/css/**/*.css')
       .pipe(concat('docs.css'))
-      .pipe(gulp.dest(outPath));
+      .pipe(gulp.dest(config.outPath));
   });
 
   // Converts HTML to JS
@@ -114,48 +117,48 @@ module.exports = function (gulp, config) {
         prefix: 'partials/'
       }))
       .pipe(concat('templates.js'))
-      .pipe(gulp.dest(outPath + '/js'));
+      .pipe(gulp.dest(config.outPath + '/js'));
   });
 
   // Compiles index template
   gulp.task('docs:index', false, function() {
     return gulp.src(appDir + '/index.html')
       .pipe(template({config: config}))
-      .pipe(gulp.dest(outPath));
+      .pipe(gulp.dest(config.outPath));
   });
 
   // Generates demo data
   gulp.task('docs:demos:data', false, function() {
-    return gulp.src('src/**/demo*/**/*')
+    return gulp.src(config.basePath + '/src/**/demo*/**/*')
       .pipe(util.parseDemoFiles(config.modulePrefix))
       .pipe(ngConstant({name: 'docsApp.demo-data'}))
       .pipe(rename('demo-data.js'))
-      .pipe(gulp.dest(outPath + '/js'));
+      .pipe(gulp.dest(config.outPath + '/js'));
   });
 
   // Copies demo files to `dist/docs/demo-partials` and prefixes CSS with demo ID as parent class.
   gulp.task('docs:demos:copy', false, function() {
-    return gulp.src('src/**/demo*/**/*')
+    return gulp.src(config.basePath + '/src/**/demo*/**/*')
       .pipe(gulpif(/.css$/, util.prefixDemoCss()))
-      .pipe(gulp.dest(outPath + '/demo-partials'));
+      .pipe(gulp.dest(config.outPath + '/demo-partials'));
   });
 
   // Concatenates demo scripts
   gulp.task('docs:demos:scripts', false, ['docs:demos:copy'], function() {
-    return gulp.src(outPath + '/demo-partials/**/*.js')
+    return gulp.src(config.outPath + '/demo-partials/**/*.js')
       .pipe(concat('docs-demo-scripts.js'))
-      .pipe(gulp.dest(outPath));
+      .pipe(gulp.dest(config.outPath));
   });
 
   gulp.task('docs:serve', 'Serves docs', function() {
     var host = argv.host || 'localhost';
     var port = argv.port || '8000';
-    gulp.src('dist')
+    gulp.src(config.outPath)
       .pipe(server({
         host: host,
         port: port,
-        fallback: 'docs/index.html',
-        open: 'http://' + host + ':' + port + '/docs'
+        fallback: config.urlPath + '/index.html',
+        open: 'http://' + host + ':' + port + config.urlPath
       }));
   }, {
     options: {
